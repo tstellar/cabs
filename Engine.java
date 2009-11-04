26982d40782987b5d939338fec8d199f76c247c5
Tom Stellard <tstellar@cs.uoregon.edu>
2009-11-04 09:52:29 -0800

    Add local variables to the Engine class for storing the overall size of the grid as well as the local size of the grid.

--- Engine.java
+++ Engine.java
@@ -1,9 +1,19 @@
-
 public class Engine {
 
 	Cell[][] cells;
+	int globalHeight;
+	int globalWidth;
+	int tlx;
+	int tly;
+	int height;
+	int width;
 
-	public Engine(int height, int width) {
+	public Engine(int tlx, int tly, int height, int width, int globalHeight, int globalWidth) {
+		this.tlx = tlx;
+		this.tly = tly;
+		this.globalHeight = globalHeight;
+		this.globalWidth = globalWidth;
+
 		cells = new Cell[height][width];
 		for (int i = 0; i < cells.length; i++) {
 			for (int j = 0; j < cells[i].length; j++) {
@@ -14,8 +24,8 @@ public class Engine {
 
 	public void go(int turn) {
 		for (Cell[] cell : cells) {
-			for (int j = 0; j < cell.length; j++) {
-				cell[j].go(turn);
+			for (Cell element : cell) {
+				element.go(turn);
 			}
 		}
 	}
@@ -25,28 +35,26 @@ public class Engine {
 		newCell.add(agent);
 		oldCell.remove(agent);
 	}
-	
-	private String getCellOwner(int x, int y){
-	}
 
-	private void discoverEngines(){
-	}
-
-	private Cell findCell(String ip, int x, int y){
-	}
-
+	/*
+	 * private String getCellOwner(int x, int y){ }
+	 * 
+	 * private void discoverEngines(){ }
+	 * 
+	 * private Cell findCell(String ip, int x, int y){ }
+	 */
 	private Cell findCell(int x, int y) {
-		if (y >= cells.length) {
-			y = y % cells.length;
+		if (y >= globalHeight) {
+			y = y % globalHeight;
 		}
-		if (x >= cells[y].length) {
-			x = x % cells.length;
+		if (x >= globalWidth) {
+			x = x % globalWidth;
 		}
 		if (y < 0) {
-			y = (y % cells.length) + cells.length;
+			y = (y % globalHeight) + globalHeight;
 		}
 		if (x < 0) {
-			x = (x % cells.length) + cells[y].length;
+			x = (x % globalWidth) + globalWidth;
 		}
 		return cells[y][x];
 	}
@@ -59,8 +67,8 @@ public class Engine {
 
 	public void print() {
 		for (Cell[] cell : cells) {
-			for (int j = 0; j < cell.length; j++) {
-				if (cell[j].agents.size() > 0) {
+			for (Cell element : cell) {
+				if (element.agents.size() > 0) {
 					System.out.print("* ");
 				} else {
 					System.out.print("- ");
@@ -72,7 +80,7 @@ public class Engine {
 
 	public static void main(String[] args) {
 
-		Engine engine = new Engine(5, 5);
+		Engine engine = new Engine(0, 0, 5, 5, 5, 5);
 		engine.placeAgents(5);
 		engine.print();
 		for (int i = 0; i < 8; i++) {
