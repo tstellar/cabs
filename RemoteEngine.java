63c84f8202d40ddeacbe0cbff3cbe297873ec008
Tom Stellard <tstellar@cs.uoregon.edu>
2009-11-04 11:37:52 -0800

    Add some method stubs to RemoteEngine class, and change its constrcutor.

--- RemoteEngine.java
+++ RemoteEngine.java
@@ -1,13 +1,25 @@
 import java.util.*;
+import java.net.Socket;
 
 
 public class RemoteEngine extends Engine{
 
-	public RemoteEngine(int tlx, int tly, int width, int height){
-		super(tlx, tly, width, height);
+	Socket socket;
+	LocalEngine localEngine;
+
+	public RemoteEngine(Socket socket, LocalEngine localEngine){
+		this.socket = socket;
+		this.localEngine = localEngine;
 	}
 	
 	public Cell findCell(int x, int y){
-		return null;
+		//TODO: Send a 'findCell' request to this remote machine using
+		//the message protocol.
+		return null;	
+	}
+
+	public void sendAgent(Cell newCell, Agent agent){
+		//TODO: Send a 'sendAgent' request to the remote machine using
+		//the message protocol.
 	}
 }
