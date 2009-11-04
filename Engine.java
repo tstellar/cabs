5a3131a3a0906edd77b26573a173df680134e3c2
Tom Stellard <tstellar@cs.uoregon.edu>
2009-11-04 11:02:11 -0800

    Create a class hierarchy for the Engine classes.  The Engine class is now an abstract class that is extended by LocalEngine and RemoteEngine.  LocalEngine is nearly identical to the old Engine class and is now the main class. LocalEngine will represent the Engine running on the local machine, and RemoteEngine will represent an Engine running on a remote machine.

--- Engine.java
+++ Engine.java
@@ -1,97 +1,24 @@
 import java.util.*;
 
 
-public class Engine{
+public abstract class Engine{
 
-
-	Cell[][] cells;
-	int globalHeight;
-	int globalWidth;
 	int tlx;
 	int tly;
-	int height;
 	int width;
+	int height;
+	
+	public abstract Cell findCell(int x, int y);
 
-	public Engine(int tlx, int tly, int height, int width, int globalHeight, int globalWidth){
+	public Engine(int tlx, int tly, int width, int height){
 		this.tlx = tlx;
 		this.tly = tly;
-		this.globalHeight = globalHeight;
-		this.globalWidth = globalWidth;
-		cells = new Cell[height][width];
-		for(int i=0; i< cells.length; i++){
-			for(int j=0; j< cells[i].length; j++){
-				cells[i][j] = new Cell(j,i,this);
-			}
-		}
+		this.width = width;
+		this.height = height;
 	}
 
-	public void go(int turn){
-		for(int i=0; i< cells.length; i++){
-			for(int j=0; j< cells[i].length; j++){
-				cells[i][j].go(turn);
-			}
-		}
-	}
-	
-	public void moveAgent(Agent agent, Cell oldCell, int x, int y){
-		Cell newCell = findCell(oldCell.x + x, oldCell.y + y);
-		newCell.add(agent);
-		oldCell.remove(agent);
-	}
-/*	
-	private String getCellOwner(int x, int y){
+	public Boolean hasCell(int x, int y){
+		return x >= tlx && y >= tly && x < tlx + width && y < tly + height;
 	}
 
-	private void discoverEngines(){
-	}
-
-	private Cell findCell(String ip, int x, int y){
-	}
-*/
-	private Cell findCell(int x, int y){
-		if(y >= globalHeight){
-			y = y % globalHeight;
-		}
-		if(x >= globalWidth){
-			x = x % globalWidth;
-		}
-		if(y < 0){
-			y = (y % globalHeight) + globalHeight;
-		}
-		if(x < 0){
-			x = (x % globalWidth) + globalWidth;
-		}
-		return cells[y][x];
-	}
-
-	public void placeAgents(int agents){
-		for(int i=0; i< agents; i++){
-			cells[i][0].add(new Rabbit());
-		}
-	}
-
-	public void print(){
-		for(int i=0; i< cells.length; i++){
-			for(int j=0; j< cells[i].length; j++){
-				if(cells[i][j].agents.size() > 0){
-					System.out.print("* ");
-				}
-				else{
-					System.out.print("- ");
-				}
-			}
-			System.out.println();
-		}
-	}	
-	
-	public static void main(String[] args){
-		
-		Engine engine = new Engine(0, 0, 5, 5, 5, 5);
-		engine.placeAgents(5);
-		engine.print();
-		for(int i=0; i< 8; i++){
-			engine.go(i);
-			engine.print();
-		}
-	}
 }
